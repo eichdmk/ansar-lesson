5d62054cde4c03c6f4663432d4c2ed38ea669794
eichdmk <imutsaev95@gmail.com>
2026-05-22 20:54:29 +0300

    fix

--- server/index.ts
+++ server/index.ts
@@ -29,8 +29,8 @@ const __dirname = path.dirname(__filename)
 const clientBuildPath = path.join(__dirname, "../client/dist");
 app.use(express.static(clientBuildPath));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(clientBuildPath, "index.html"));
+app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(clientBuildPath, "index.html"));
 });
 
 const storage = multer.diskStorage({
